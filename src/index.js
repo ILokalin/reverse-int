6713bdf232ab1bfa91c13f14704fe422ea94958e
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-20 07:22:14 +0500

    feat: create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  let result = 0;
+  let checkNumber = Math.abs(n)
+
+  while (checkNumber > 0) {
+    result = result * 10 + checkNumber % 10;
+    checkNumber = Math.floor(checkNumber / 10);
+  }
   
+  return result;
 }
